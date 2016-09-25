c0af4ac97dcdcf5685ff21d34c5fd4bae495010f
Isil <alcon678@gmail.com>
2016-09-25 04:10:16 +0200

    Fibonacci Recursion

---
+++ src/Fibonacci.java
@@ -0,0 +1,23 @@
+
+public class Fibonacci {
+
+	public final static int N0=0;
+	public final static int N1=1;
+	
+
+	public static  int suma(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	public static int fibonacci(int iteration) {	
+		if(iteration == 0){
+			return N0;
+		}
+		else if(iteration==1){
+			return N1;
+		}
+		int result =suma(fibonacci(iteration-1), fibonacci(iteration-2));
+		return result;
+	}
+
+}
---
+++ src/Test.java
@@ -0,0 +1,57 @@
+import static org.junit.Assert.*;
+
+
+public class Test {
+
+	
+	@org.junit.Test
+	public void N0ShouldBeCero(){
+		assertEquals(0, Fibonacci.N0);
+	}
+	
+	@org.junit.Test
+	public void N1ShouldBeOne(){
+		assertEquals(1, Fibonacci.N1);
+	}
+	
+	@org.junit.Test
+	public void N0plusN1shouldBeOne(){
+		assertEquals(1,Fibonacci.suma(Fibonacci.N0, Fibonacci.N1));
+	}	
+	
+	@org.junit.Test
+	public void calculateFibonacciOfThree(){
+		int n=3;
+		
+		assertEquals(2,Fibonacci.fibonacci(n));
+	}
+	
+	@org.junit.Test
+	public void calculateFibonacciOfCero(){
+		int n=0;
+		
+		assertEquals(0,Fibonacci.fibonacci(n));
+	}
+	
+	@org.junit.Test
+	public void calculateFibonacciOfOne(){
+		int n=1;
+		
+		assertEquals(1,Fibonacci.fibonacci(n));
+	}
+
+	@org.junit.Test
+	public void calculateFibonacciOfTwelve() {
+		int n = 12;
+		
+		assertEquals(144, Fibonacci.fibonacci(n));
+	}
+
+	@org.junit.Test
+	public void calculateFibonacciOfFive() {
+		int n = 5;
+		
+		assertEquals(5, Fibonacci.fibonacci(n));
+	}
+
+}
